cb23aedbb82fba16d4bade7a3eb04ae3e59c0533
Augusto Dias <me@gutodias.dev>
2022-05-05 07:48:11 -0300

    feat: add first project setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
-import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
