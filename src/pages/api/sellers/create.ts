4d0a320551e94bc3eff3173c8faf800b7db424a6
Augusto Dias <me@gutodias.dev>
2022-05-13 08:49:15 -0300

    refactor: using then syntax to create seller

--- src/pages/api/sellers/create.ts
+++ src/pages/api/sellers/create.ts
@@ -18,14 +18,16 @@ handler.post(async (req, res) => {
   await prisma.seller.create({
     data: {
       business_name: business_name,
-      seller_profile: {
-        create: {
-          business_email: business_email,
-          business_phone: business_phone,
-          location_mark: location_mark
-        }
-      }
     }
+  }).then(async (seller) => {
+    await prisma.sellerProfile.create({
+      data: {
+        seller_id: seller.id,
+        business_email: business_email,
+        business_phone: business_phone,
+        location_mark: location_mark
+      }
+    })
   })
 
   return res.status(201).end()
